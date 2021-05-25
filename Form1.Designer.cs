b6c1c798647927292e04d234e38aa66f66cde5f6
Daniyal Shah <daniyalshah7862018@gmail.com>
2021-05-25 22:52:15 +0500

    Assignment

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerpicturebox = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(52, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(707, 317);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timerpicturebox
+            // 
+            this.timerpicturebox.Interval = 1000;
+            this.timerpicturebox.Tick += new System.EventHandler(this.timerpicturebox_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timerpicturebox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+        List<String> images = new List<string>();
+        int count=0;
+
+        private void timerpicturebox_Tick(object sender, EventArgs e)
+        {
+               this.pictureBox1.Image = Image.FromFile(images[count]);
+               count=(count+1)%3;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            XmlTextReader xReader = new XmlTextReader(@"C:\Users\92306\Desktop\Assignment\Paths.xml");
+
+             while (xReader.Read())
+            {
+                if (xReader.NodeType== XmlNodeType.Text)
+                {                    
+                         Console.WriteLine(xReader.Value);
+                         images.Add(""+xReader.Value);
+                }
+            }
+            timerpicturebox.Enabled = true;
+
+            
+
+        }
+
+
+
+
+    }
+}
